02107eef745db99f29a426f0ac59bc9d09ce9470
shey-kail <shey-kail@outlook.com>
2025-03-25 11:48:38 +0800

    单独写出阴阳和五行两个模块，另外更改Relationship的两个方法的返回值类型

--- src/traits/mod.rs
+++ src/traits/mod.rs
@@ -1,5 +1,6 @@
 //! 特征模块，定义命理学中常用的特征
-
+pub mod yinyang_wuxing;
+use yinyang_wuxing::*;
 /// 中文名称特征，用于获取事物的中文名称
 pub trait ChineseName {
     /// 获取中文名称
@@ -27,14 +28,15 @@ pub trait Iter {
 pub trait Relationship {
     type Item;
     /// 获取我对目标的关系（双方关系）
-    fn relationship_with(&self, other: &Self) -> Self::Item;
+    fn relationship_with(&self, other: &Self) -> Vec<Self::Item>;
     /// 根据给定的关系反向推导出对应的目标
-    fn from_relationship(&self, relationship: Self::Item) -> Self;
+    fn from_relationship(&self, relationship: Self::Item) -> Option<Self> where Self: Sized;
 }
 
 /// 三方关系特征，用于描述事物间的三方关系
 pub trait TripleRelationship {
     type Item;
     /// 获取与两个目标的关系（三方关系）
-    fn relationship_with_triple(&self, second: &Self, third: &Self) -> Self::Item;
+    fn relationship_with_triple(&self, second: &Self, third: &Self) -> Option<Self::Item>;
+    fn from_relationship_triple(&self, relationship: Self::Item) -> (Option<Self>, Option<Self>) where Self: Sized;
 }
