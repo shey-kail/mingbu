6a0772880ea9528da874b6ee39c14ba7ba0b71ce
Shey Kail <sheykail132@gmail.com>
2025-10-24 20:16:45 +0800

    清空文件，重新设计架构

--- src/lib.rs
+++
@@ -1,9 +0,0 @@
-//! 命理学库的主要入口
-
-mod symbol_system;
-mod time;
-mod traits;
-
-pub use symbol_system::{EarthlyBranch, HeavenlyStem, Trigram, WuXing, YinYang};
-pub use time::{DateTime};
-pub use traits::{ChineseName, Index, Iter, Relationship, TripleRelationship};
--- src/time/mod.rs
+++
@@ -1,5 +0,0 @@
-//! 时间模块，包含农历、公历和星历相关的基本类型和特征
-
-pub mod date_time;
-
-pub use date_time::DateTime;
