b316f6854dce6ada5f3c1009216745b6ebe5f353
Shey Kail <sheykail132@gmail.com>
2025-10-27 18:59:01 +0800

    fix: implement Debug, Display and Error traits for MingbuError to fix test compilation

--- src/json.rs
+++ src/json.rs
@@ -1,10 +1,19 @@
 use serde::Serialize;
+use std::fmt;
 
-#[derive(Serialize)]
+#[derive(Debug, Serialize)]
 pub struct MingbuError {
     pub code: &'static str,
     pub message: String,
 }
+
+impl fmt::Display for MingbuError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "MingbuError {}: {}", self.code, self.message)
+    }
+}
+
+impl std::error::Error for MingbuError {}
 
 pub fn to_json<T: Serialize>(result: &T) -> Result<String, MingbuError> {
     serde_json::to_string(result)
